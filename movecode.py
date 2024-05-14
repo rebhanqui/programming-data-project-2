15ddf68134029b6c2ab23fe979ce01674da1c961
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-05-14 19:42:07 +0100

    rearranged notebook

---
+++ movecode.py
@@ -0,0 +1,150 @@
+# Main Title
+
+
+# Preprocessing Data
+## Import Packages
+#importing packages required for analysis and visualization
+import requests
+from io import StringIO
+from IPython.display import display
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+## Reading in Data and initital analysis functions
+
+### CO2
+    #data 1
+
+    #data 2
+
+
+    # 
+
+
+
+
+### CH4
+    #data 1
+
+    
+    #data2
+
+
+
+
+
+### TEMPERATURE
+
+
+### RAINFALL
+
+## Initial Data Adjustments
+
+### Cleaning Data as required
+
+#### CO2
+
+
+
+
+##### Merge Data
+
+
+#### CH4
+
+
+##### Merge Data
+
+
+#### TEMPERATURE
+
+
+##### Merge Data
+
+
+## Statistical Measurements
+
+### CO2
+
+
+### CH4
+
+
+### TEMPERATURE
+
+
+## Irish Context
+
+
+#### TEMPERATURE
+
+
+##### Merge Data
+
+
+#### RAINFALL
+
+##### Merge Data
+
+
+## Analysis - Trends & Relationships
+
+### CO2
+
+#1
+#Matplotlib Line plot
+
+
+#2
+#size
+
+
+### CH4
+
+#### Plot
+#1
+#SNS Lineplot
+
+
+#2
+
+
+#3
+
+
+
+### TEMPERATURE
+
+#### Plot
+#1
+
+
+#2
+
+
+
+### RAINFALL
+
+#### Plot
+#1
+
+
+
+
+#2
+
+
+
+### Plot Overview
+
+
+## Predictions
+
+### Synthetic Data
+
+### Comparisons with public published data
+
+
+## References
